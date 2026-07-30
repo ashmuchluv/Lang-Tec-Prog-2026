cf6d78e2cb22d4a82836d6dc47409934097a750e
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-07-29 21:40:23 -0300

    Enhance comment on printf usage in HelloWorld.c
    Expanded comment to clarify the use of '\n' in C.

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main (){
-  printf("Hello World!\n"); // \n é um caractere para finalizar a linha do texto na linguagem C.
+  printf("Hello World!\n"); // \n é um caractere para finalizar a linha do texto na linguagem C. É o caractere que representa um "enter".
   return 0;
 }
