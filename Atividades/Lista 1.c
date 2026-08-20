b782db2678433a99209be4655275d7093515726b
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-08-19 21:20:47 -0300

    Create Lista 1.c

---
+++ Atividades/Lista 1.c
@@ -0,0 +1,21 @@
+// Lista de exercicios 1
+// 1 - 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a, b;
+	
+	printf("Adicione o primeiro numero: ");
+	scanf("%d", &a);
+	
+	printf("Adicione o segundo numero: ");
+	scanf("%d", &b);
+	
+	printf("Os numeros são: %d e %d\n", b, a);
+	
+	return 0;
+}
+
+// 2 - 
