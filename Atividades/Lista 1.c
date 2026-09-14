674561cf644e538bb54ef8dd05ba287b580c0b9a
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-14 13:35:40 -0300

    Implement scientific notation converter
    Added a program to convert a positive number to scientific notation.

--- Atividades/Lista 1.c
+++ Atividades/Lista 1.c
@@ -19,3 +19,28 @@ int main(int argc, char *argv[]) {
 }
 
 // 2 - 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    double numero;
+    int expoente = 0;
+
+    printf("Digite um valor positivo: ");
+    scanf("%lf", &numero);
+
+    while(numero >= 10) {
+        numero = numero / 10;
+        expoente++;
+    }
+
+    while(numero < 1) {
+        numero = numero * 10;
+        expoente--;
+    }
+
+    printf("%.2lf x 10^%d\n", numero, expoente);
+
+    return 0;
+}
