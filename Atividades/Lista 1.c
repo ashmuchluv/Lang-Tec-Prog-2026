7c445df04e5a314390f56c56aef1b610aee68607
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-14 13:40:40 -0300

    Add multiple C programs for various calculations

--- Atividades/Lista 1.c
+++ Atividades/Lista 1.c
@@ -44,3 +44,80 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+// 3 - 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+ int n, resto;
+
+    printf("Digite um numero entre 1 e 64: ");
+    scanf("%d", &n);
+
+    printf("Binario: ");
+
+    while(n > 0) {
+        resto = n % 2;
+        n = n / 2;
+
+        printf("%d", resto);
+    }
+
+    return 0;
+}
+
+// 4 - 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+   double salario, vendas, total;
+
+    printf("Digite o salario fixo: ");
+    scanf("%lf", &salario);
+
+    printf("Digite o valor total das vendas: ");
+    scanf("%lf", &vendas);
+
+    total = salario + (vendas * 0.15);
+
+    printf("TOTAL = R$ %.2lf\n", total);
+
+    return 0;
+}
+
+// 5 - 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    double num1, num2, num3, num4;
+    double soma, media, produto;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%lf", &num1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%lf", &num2);
+
+    printf("Digite o terceiro valor: ");
+    scanf("%lf", &num3);
+
+    printf("Digite o quarto valor: ");
+    scanf("%lf", &num4);
+
+    soma = num1 + num2 + num3 + num4;
+    media = soma / 4;
+    produto = num1 * num2 * num3 * num4;
+
+    printf("Soma = %.2lf\n", soma);
+    printf("Media = %.2lf\n", media);
+    printf("Produto = %.2lf\n", produto);
+
+
+    return 0;
+}
