64605169ff832942bd76e3856b5baa8ee9d8e755
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-14 22:24:25 -0300

    Update and rename 14.09.26_permutação de 4 numeros to 14.09.26.c

---
+++ 14.09.26.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int n1, n2, n3, n4, aux;
+	
+	printf ("Adicione os 4 numeros no formato: X X X X: \n");
+	scanf ("%d %d %d %d", &n1, &n2, &n3, &n4);
+	
+	aux = n1;
+	
+	n1 = n3;
+	n3 = n4;
+	n4 = n2;
+	n2 = aux;
+	
+	printf ("%d, %d, %d, %d", n1, n2, n3, n4);
+	
+	return 0;
+}
+
+// 2 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+float PVP, VPA, valorpatrimonial, acoesdisponiveis, valoratualacao;
+
+	printf ("\nAdicione o valor patrimonial: ");
+	scanf ("%f", &valorpatrimonial);
+	
+	printf ("\nAdicione a quantidade de acoes disponiveis: ");
+	scanf ("%f", &acoesdisponiveis);
+	
+	printf ("\nAdicione o valor atual da acao em R$: ");
+	scanf ("%f", &valoratualacao);
+	
+	VPA = 
+
+	return 0;
+}
